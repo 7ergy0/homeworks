85ac6b7ac6791b6c1f35cc2eb326a877cf90af67
Сергей <segeshapluk@gmail.com>
2022-02-17 10:21:53 +0300

    hw13(error fix)

--- src/p2-homeworks/h13/request-api/RequestsApi.ts
+++ src/p2-homeworks/h13/request-api/RequestsApi.ts
@@ -2,10 +2,11 @@ import axios from "axios";
 
 
 
+
 export const requestsApi={
     request(success:boolean){
-        return axios.post("https://neko-cafe-back.herokuapp.com/auth/test",{body:success})
-            .then(response=> alert(response))
+        return axios.post("https://neko-cafe-back.herokuapp.com/auth/test",{success})
+            .then(response=> alert(JSON.stringify(response.data)))
 
             .catch(error=>alert(error.response?error.response.data.errorText:error.message))
     },
